231e8b2cb9cac7b3195a97eef7b848bce74a8b87
duda973 <duda.todorovic.97@gmail.com>
2018-03-29 17:10:48 +0200

    Dokumentovana je klasa VulkanizerskaRadnja

--- VulkanizerskaRadnja/src/gume/radnja/VulkanizerskaRadnja.java
+++ VulkanizerskaRadnja/src/gume/radnja/VulkanizerskaRadnja.java
@@ -3,9 +3,26 @@ package gume.radnja;
 import java.util.LinkedList;
 import gume.AutoGuma;
 
+
+/**
+ * Klasa koja predstavlja vulkanizersku radnju
+ * @author Dragutin Todorovic
+ * @version 0.1 Sadrzi atribut lista guma, metode dodajGumu i pronadjiGumu
+ *
+ */
 public class VulkanizerskaRadnja {
+	
+	/**
+	 * Lista guma
+	 */
 	private LinkedList<AutoGuma> gume = new LinkedList<AutoGuma>();
 
+	/**
+	 * Dodaje novu gumu u listu guma
+	 * @param a Guma koju treba dodati u listu
+	 * @throws java.lang.RuntimeException Ako uneta guma vec postoji u listi
+	 * @throws java.lang.NullPointerException Ako je uneta guma null
+	 */
 	public void dodajGumu(AutoGuma a) {
 		if (a == null)
 			throw new NullPointerException("Guma ne sme biti null");
@@ -14,6 +31,12 @@ public class VulkanizerskaRadnja {
 		gume.addFirst(a);
 	}
 
+	/**
+	 * Pronalazi gumu u listi guma ukoliko postoji
+	 * @param markaModel Naziv marke gume i modela gume
+	 * @return novaLista vraca gumu koju pronadje kao listu
+	 * @return null ukoliko je uneta vrednost markaModel null
+	 */
 	public LinkedList<AutoGuma> pronadjiGumu(String markaModel) {
 		if (markaModel == null)
 			return null;
